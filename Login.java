37c857d1e4e446d3dd01bc8301acc3f9c5500d54
PuspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2026-03-15 10:37:23 +0530

    Credential Parts

---
+++ Login.java
@@ -0,0 +1,7 @@
+public class Login {
+   public static void main(String args[]) {
+      String username="Puspendra Jaiswal";
+      String password="*******";
+      System.out.println(username+"-"+password);
+   }
+}
